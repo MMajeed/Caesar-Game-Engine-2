617a9554b4885812b090ba321f39384e334730ee
Mohammed <mmajeed668@gmail.com>
2013-11-26 12:03:06 -0500

    Issues with github, deleting everything

--- CaesarHelperLibrary/GenerateGUID.cpp
+++
@@ -1,9 +0,0 @@
-#include "GenerateGUID.h"
-#include "Converter.h"
-#include <boost/uuid/uuid_generators.hpp> // generators
-#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
-
-std::string CHL::GenerateGUID()
-{
-	return CHL::ToString(boost::uuids::random_generator()());
-}
--- ForeignLibrary/Lua.hpp
+++
@@ -1,5 +0,0 @@
-extern "C" {
-#include <lua5.1.5\lua.h>
-#include <lua5.1.5\lualib.h>
-#include <lua5.1.5\lauxlib.h>
-}
